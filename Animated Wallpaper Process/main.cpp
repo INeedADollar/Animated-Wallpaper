a45a9cfa29e17f68a234352dc46757ace792220c
INeedADollar <58915954+INeedADollar@users.noreply.github.com>
2020-10-14 20:35:03 +0300

    Update main.cpp

--- Animated Wallpaper Process/main.cpp
+++ Animated Wallpaper Process/main.cpp
@@ -1,3 +1,23 @@
+/*******************************************************************************
+MIT License
+Copyright (c) 2020 INeedADollar
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+*******************************************************************************/
+
 #include "mainwindow.h"
 #include "processthread.h"
 
@@ -6,6 +26,13 @@
 #include <QApplication>
 #include <QMessageBox>
 #include <QSysInfo>
+
+/*******************************************************************************
+    bool vectorContains(std::vector<std::string>, std::string)
+    
+    Function used for checking if list of arguments (the vector) contains a 
+    specific argument.
+*******************************************************************************/
 
 bool vectorContains(std::vector<std::string> v, std::string str){
     for (size_t i = 1; i < v.size(); ++i) {
@@ -16,6 +43,14 @@ bool vectorContains(std::vector<std::string> v, std::string str){
 
     return false;
 }
+
+/*******************************************************************************
+    int main(int argl, char *argc[])
+    
+    Main function of the programing.
+    Function used for checking if this process was started by 
+    AnimatedWallpaper.exe and for starting the work of this process.
+*******************************************************************************/
 
 int main(int argl, char *argc[]){
     std::vector<std::string> v(argc, argc+argl);
